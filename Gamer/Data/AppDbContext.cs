468932b01691c9502c77e651331e3a66f99aea41
sulaiman-i13 <157150037+sulaiman-i13@users.noreply.github.com>
2024-11-17 17:07:35 +0300

    seed data

--- Gamer/Data/AppDbContext.cs
+++ Gamer/Data/AppDbContext.cs
@@ -9,9 +9,26 @@ namespace Gamer.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Device> Devices { get; set; }
         public DbSet<GameDevice> GameDevices { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<GameDevice>().HasKey(k => new { k.GameId, k.DeviceId });
+
+            modelBuilder.Entity<Category>().HasData([
+                new Category{Id=1,Name="Sports"},
+                new Category{Id=2,Name="Action"},
+                new Category{Id=3,Name="Adventure"},
+                new Category{Id=4,Name="Racing"},
+                new Category{Id=5,Name="Fighting"},
+                new Category{Id=6,Name="Film"},
+                ] );
+            modelBuilder.Entity<Device>().HasData([
+                new Device{Id=1,Name="PlayStation",Icon="bi bi-playstation"},
+                new Device{Id=2,Name="Xbox",Icon="bi bi-xbox"},
+                new Device{Id=3,Name="Nintendo Switch",Icon="bi bi-nintendo-switch"},
+                new Device{Id=4,Name="Pc",Icon="bi bi-pc-display"},
+
+                ]);
         }
     }
 }
